a8f9508e8eae3dbf34eb0498a7c3c50c1d38cb24
RexFullerton <rexfullerton9@gmail.com>
2024-02-12 09:01:50 -0500

    Initial commit

---
+++ questions.py/Fizzbuzz.py
@@ -0,0 +1,10 @@
+def FizzBuzz():
+    for i in range(1,101,1):
+        if i % 3 == 0 and i % 5 != 0:
+            print("fizz")
+        elif i % 5 == 0 and i % 3 !=0:
+            print("buzz")
+        elif i % 5 == 0 and i % 3 ==0:
+            print("fizzbuzz")
+        else:
+            print(i)
